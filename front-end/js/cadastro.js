388f83f89df32c5c3cb0fcbdfe94352dc87975f5
tiago-sb <belatiago72@gmail.com>
2024-11-18 22:12:44 -0300

    feat(front-end): modificação da estrutura visual do projeto

--- front-end/js/cadastro.js
+++ front-end/js/cadastro.js
@@ -1,3 +1,50 @@
+document.addEventListener("DOMContentLoaded", carregarPaises)
+document.addEventListener("DOMContentLoaded", carregarEstados)
+
+async function carregarPaises() {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all")
+    const paises = await response.json()
+
+    const paises_ordenados = paises.sort((a, b) =>
+      a.name.common.localeCompare(b.name.common)
+    )
+
+    paises_ordenados.forEach((pais) => {
+      const option = document.createElement("option")
+      option.value = pais.cca2
+      option.textContent = pais.name.common
+      document.getElementById("pais").appendChild(option)
+    })
+
+  } catch (error) {
+    console.error("Erro ao carregar países")
+  }
+}
+
+async function carregarEstados() {
+  try {
+    const response = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    const estados = await response.json()
+
+    
+    const estados_ordenados = estados.sort((a, b) =>
+      a.nome.localeCompare(b.nome)
+    )
+
+    estados_ordenados.forEach((estado) => {
+      const option = document.createElement("option")
+      option.value = estado.sigla
+      option.textContent = estado.nome
+      document.getElementById("estado").appendChild(option)
+    })
+
+    return estado.nome
+  } catch (error) {
+    console.error("Erro ao carregar estados")
+  }
+}
+
 // preencher o option dos dias
 const dia = document.getElementById("dia")
 for (let i = 1; i <= 31; i++) {
@@ -10,7 +57,7 @@ for (let i = 1; i <= 31; i++) {
 // Preencher o option dos anos (1900 - ano atual)
 const ano = document.getElementById("ano")
 const ano_atual = new Date().getFullYear()
-for (let i = ano_atual; i >= 0; i--) {
+for (let i = ano_atual; i >= 1900; i--) {
   const option = document.createElement("option")
   option.value = i
   option.textContent = i
